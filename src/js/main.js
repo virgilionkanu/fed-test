b5ce6d06d5e365ef0ddba96149ed471201e09210
virgilionkanu <virgiliocano53@gmail.com>
2022-08-10 22:15:18 -0300

    fix: bug on file main.js line 9 solved - it was an unnecessary string

--- src/js/main.js
+++ src/js/main.js
@@ -5,8 +5,7 @@ var reportsWidget = {
             '{{#.}}' +
                 '<article class="reports_item">' +
                     '<a href="{{cover}}" target="_blank">' +
-                        '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>'
-                    '</a>' +
+                        '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>' +
                     '<footer class="reports_docs">' +
                         '{{#documents}}' +
                             '<h3 class="reports_title">' +
